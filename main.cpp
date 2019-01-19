b4701855bc00f5029c26999d9cac409fa1d0e833
LostInKadath <forgotten.in.kadath@gmail.com>
2019-01-19 20:41:51 +0300

    Vcxproj created

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@
 int main(void)
 {	
 	FileGenerator gen;
-//	gen.GenerateTextFile("testdata.txt");
-//	gen.GenerateBinaryFile("testdata.bin");
+	gen.GenerateTextFile("data/testdata.txt");
+	gen.GenerateBinaryFile("data/testdata.bin");
 
 	char *flow = new char[gen.flowSize];
 
-	std::ifstream in("testdata.bin");
+	std::ifstream in("data/testdata.bin");
 	in.read((char*)&*flow, gen.flowSize * sizeof(char));
 	in.close();
 	
@@ -23,11 +23,13 @@ int main(void)
 	ICallback *callback = new Callback();
 	IReceiver *receiver = new Receiver(callback);
 
-	try {
+	try
+	{
 		for (unsigned int i = 0; i < gen.flowSize; i += gen.blockSize)
 			receiver->Receive(flow + i, gen.blockSize);
 	}
-	catch (std::exception &e) {
+	catch (std::exception &e)
+	{
 		std::cerr << e.what() << '\n';
 	}
 	
